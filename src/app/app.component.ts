1e3316fb42f7216c699323cb5dcffa472abac532
ivan_benegiamo <ivan.benegiamo@revevol.eu>
2017-10-09 17:38:58 +0200

    app completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from './article/article.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  articles: Article[];
+
+    constructor() {
+      this.articles = [
+        new Article('Google article', 'http://www.google.it', 4),
+        new Article('Google article', 'http://www.google.it', 4),
+        new Article('Google article', 'http://www.google.it', 4)
+      ]
+    }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value}and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+// material
+import {
+  MdToolbarModule,
+  MdCardModule,
+  MdFormFieldModule,
+  MdInputModule,
+  MdButtonModule
+  // MdButtonModule
+} from '@angular/material';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdCardModule,
+    MdFormFieldModule,
+    MdInputModule,
+    MdButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
